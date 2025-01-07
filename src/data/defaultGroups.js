5b6a3a72b2ca91ff98e4a1314845094cf840e476
Mike Jovanovich <mpjovanovich@gmail.com>
2025-01-07 12:07:10 -0500

    Update default groups. Discussed course sequencing with Justin; this seems to be in pretty good shape.

--- src/data/defaultGroups.js
+++ src/data/defaultGroups.js
@@ -2,32 +2,32 @@ export const defaultGroups = [
   // F1
   { id: "INFM109", parentId: "F1" },
   { id: "SDEV120", parentId: "F1" },
-  { id: "SVAD150", parentId: "F1" },
-  { id: "VISC104", parentId: "F1" },
 
   // F2
-  { id: "SDEV153", parentId: "F2" },
   { id: "SDEV140", parentId: "F2" },
-  { id: "VISC202", parentId: "F2" },
+  { id: "VISC104", parentId: "F2" },
 
   // S3
-  { id: "DBMS110", parentId: "S3" },
-  { id: "SDEV155", parentId: "S3" },
+  { id: "SDEV153", parentId: "S3" },
+  { id: "VISC202", parentId: "S3" },
 
   // S4
-  { id: "SDEV2xx", parentId: "S4" },
+  { id: "SDEV155", parentId: "S4" },
+  { id: "DBMS110", parentId: "S4" },
 
   // F5
+  { id: "SDEV2xx", parentId: "F5" },
   { id: "SDEV255", parentId: "F5" },
 
   // F6
   { id: "SDEV257", parentId: "F6" },
+  { id: "SVAD150", parentId: "F6" },
 
   // S7
+  { id: "CPIN269", parentId: "S7" },
   { id: "SDEV265", parentId: "S7" },
 
   // S8
-  { id: "CPIN269", parentId: "S8" },
   { id: "CPIN279", parentId: "S8" },
 
   // Uncategorized
